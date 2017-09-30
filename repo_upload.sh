883d3940c151560a91194899edfc66612abf1035
Gijs Molenaar <gijs@pythonic.nl>
2017-09-30 10:13:13 +0200

    fix issue #19

--- repo_upload.sh
+++ repo_upload.sh
@@ -8,13 +8,27 @@ KEY=9BF9BF69E5DDE77F5ABE20DC966A924CE91888D2
 DIST=stretch
 REPO=eduvpn
 
+# remove any old local repository
 aptly publish drop ${DIST} || true
 aptly repo drop ${REPO} || true
 
+# create a new repository
 aptly repo create -architectures="amd64,i386" ${REPO}
+
+# add the binary packages 
 aptly repo add ${REPO} build/*.deb
-aptly publish -distribution=${DIST} -architectures="amd64,i386" repo ${REPO}
+
+# add the source packages
+aptly repo add ${REPO} build/*.dsc
+
+# localy publish the repository
+#aptly publish -gpg-key=${KEY} -distribution=${DIST} -architectures="amd64,i386" repo ${REPO}
+aptly publish -gpg-key=${KEY} -distribution=${DIST} -architectures="amd64,i386,all,source" repo ${REPO}
 #aptly publish update -gpg-key=${KEY} stretch
+
+# export the key which was used during packaging
 gpg --export --armor ${KEY} > ~/.aptly/public/${REPO}.key
+
+# publish the repo online
 rsync -4rv --del ~/.aptly/public/* gimo@static.eduvpn.nl:/var/www/html/web/repo/debian
 
